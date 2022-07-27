e39b32ac3a13bcaf48b4b36e2bd748b5572c8c79
kanwarmamta <k.mamta2k4@gamil.com>
2022-07-27 21:33:22 +0530

    added subtraction operator

--- calculator.c
+++ calculator.c
@@ -10,6 +10,14 @@ int main()
     printf("Enter the operation you want to proceed with from ' +,-,*,/'\n");
     scanf(" %c", &operation);
     // switch case for choosing between different operations
-    
+    switch (operation)
+    {
+    case '+':
+        printf("%f+%f=%f", num1, num2, num1 + num2);
+        break;
+    case '-':
+        printf("%f-%f=%f", num1, num2, num1 - num2);
+        break;
+    }
     return 0;
 }
