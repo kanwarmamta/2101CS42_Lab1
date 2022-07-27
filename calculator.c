8c1bc40c4a8160661976da6f181f335498693f38
kanwarmamta <k.mamta2k4@gamil.com>
2022-07-27 21:23:35 +0530

    1st commit: taking inputs from user

--- calculator.c
+++ calculator.c
@@ -1,18 +1,15 @@
-#include<stdio.h> 
+#include <stdio.h>
 
-int main(){ 
-    float num1,num2;
+int main()
+{
+    float num1, num2;
     char operation;
     // Taking input numbers and letting user choose desired operation
     printf("Enters the two numbers:\n");
-    scanf("%f %f", &num1,&num2);
+    scanf("%f %f", &num1, &num2);
     printf("Enter the operation you want to proceed with from ' +,-,*,/'\n");
     scanf(" %c", &operation);
     // switch case for choosing between different operations
-    switch(operation){
-        case '+':
-        printf("%f+%f=%f", num1,num2,num1+num2);
-        break;
-    }
-     return 0;
+    
+    return 0;
 }
