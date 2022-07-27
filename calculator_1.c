6c00dc5185d21ab99b3a8b531c61e946bd95d41d
kanwarmamta <k.mamta2k4@gamil.com>
2022-07-27 20:14:39 +0530

    Second commit: added addition operator

--- calculator_1.c
+++ calculator_1.c
@@ -8,5 +8,11 @@ int main(){
     scanf("%f %f", &num1,&num2);
     printf("Enter the operation you want to proceed with from ' +,-,*,/'\n");
     scanf(" %c", &operation);
+    // switch case for choosing between different operations
+    switch(operation){
+        case '+':
+        printf("%f+%f=%f", num1,num2,num1+num2);
+        break;
+    }
      return 0;
 }
