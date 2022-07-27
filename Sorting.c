681e7854d7c7f31a2b061081ae8561ed5e36620f
kanwarmamta <k.mamta2k4@gamil.com>
2022-07-27 22:24:35 +0530

    Second commit: adding insertion sort function

--- Sorting.c
+++ Sorting.c
@@ -9,7 +9,23 @@ void print_array(int array[], int size)
     }
     printf("\n");
 }
-
+// Insertion sort Function
+void insertion_sort(int array[], int size)
+{
+    for (int step = 1; step < size; step++)
+    {
+        int key = array[step];
+        int j = step - 1;
+        // Comparing key value with each element on the left of it until an element smaller than it is found.
+        // For descending order, change key<array[j] to key>array[j].
+        while (key < array[j] && j >= 0)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
 
 int main(void)
 {
@@ -33,6 +49,14 @@ int main(void)
 
     printf("Unsorted Array\n");
     print_array(arr, n);
-    
+    switch (sort)
+    {
+    case 1:
+        insertion_sort(arr, n);
+        printf("Sorted array in ascending order through Insertion sort:\n");
+        print_array(arr, n);
+        break;
+    }
+
     return 0;
 }
