fe22961438e5e952f767814202f6ec8ccc92b830
kanwarmamta <k.mamta2k4@gamil.com>
2022-07-27 22:53:05 +0530

    Fifth commit: adding merge sort function

--- Sorting.c
+++ Sorting.c
@@ -87,6 +87,74 @@ void bubble_sort(int array[], int size)
         }
     }
 }
+// Merge Function for Merge Sorting
+void merge(int arr[], int p, int q, int r)
+{
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    // Until we reach either end of either L or M, pick larger among elements L and M and place them in the correct position at A[p..r]
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    // When we run out of elements in either L or M, pick up the remaining elements and put in A[p..r]
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge Sort Function
+void merge_sort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+
+        // dividing array into two sub-arrays
+        int m = l + (r - l) / 2;
+
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+
+        // Merge the sorted subarrays
+        merge(arr, l, m, r);
+    }
+}
 
 int main(void)
 {
@@ -125,6 +193,11 @@ int main(void)
     case 3:
         bubble_sort(arr, n);
         printf("Sorted Array in Ascending Order through Bubble Sort:\n");
+        print_array(arr, n);
+        break;
+    case 4:
+        merge_sort(arr, 0, n - 1);
+        printf("Sorted Array in Ascending Order through Merge Sort:\n");
         print_array(arr, n);
         break;
     }
